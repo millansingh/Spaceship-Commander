8fe3dc100cda774a15e951190fb35e911fe98431
amedawar888 <amedawar888@gmail.com>
2017-12-05 02:08:21 -0700

    #1: Creates Extinguishing Panel

---
+++ src/ui/modules/SystemExtinguishPanel.java
@@ -0,0 +1,36 @@
+package ui.modules;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import game.Ship;
+import game.SpaceshipGame;
+import systems.Part;
+import ui.components.RichSlider;
+
+
+public class SystemExtinguishPanel extends Module {
+
+	private Part system;
+	private SpaceshipGame parent;
+	private RichSlider extSlider;
+	private JLabel fireIconL, fireExtinguishL;
+	private ImageIcon fireIcon = new ImageIcon("img/icons/icon_Fire.png","On Fire"),
+			fireNIcon = new ImageIcon("img/icons/icon_FireN.png","Not On Fire");
+	
+	private ImageIcon extinguishIcon = new ImageIcon("img/icons/icon_Extinguisher.png","Extinguishers Activated"),
+			extinguishNIcon = new ImageIcon("img/icons/icon_ExtinguisherN.png","Extinguishers Deactivated");
+	
+	public SystemExtinguishPanel(Ship own, int partNum,SpaceshipGame g) {
+		super(own);
+		parent = g;
+		system = owner.getPartNum(partNum);
+		JLabel ext = new JLabel("Extinguish Energy:");
+		extSlider = new RichSlider(parent,0,owner.getMaxExtinguishers(system),0,owner.getMaxExtinguishers(system)/5,owner.getMaxExtinguishers(system)/25);
+		extSlider.setValue(system.extinguishEnergy);
+		fireIconL = new JLabel(String.valueOf(system.fireDamage),fireIcon,JLabel.LEFT);
+		fireExtinguishL = new JLabel(extinguishNIcon);
+		// TODO Auto-generated constructor stub
+	}
+
+}
